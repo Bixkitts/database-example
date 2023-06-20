519b5ccff787e5312c678e184d9fc8b686e97a88
Sean Bikkes <bixkitts@gmail.com>
2023-06-20 20:59:56 +0200

    fixed http message length (buffer overflow)

--- backend/src/communication.c
+++ backend/src/communication.c
@@ -67,8 +67,7 @@ static void handleTCPPacket(char* data, uint16_t size, Client* remotehost)
     {
         setClientPacketHandler(remotehost, handleHTTPPacket);
         char* responseOK = "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE\r\nAccess-Control-Allow-Headers: Content-Type, Authorization\r\n\r\n";
-        sendDataTCP(responseOK, 174, remotehost);
-        unsetListening(remotehost);
+        sendDataTCP(responseOK, 164, remotehost);
     }
 }
 
